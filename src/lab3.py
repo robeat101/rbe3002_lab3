49e955c8cf6855f7f52fc0f693ccc196ef3c09db
Rohit Bansal <rohit@bansalrohit.com>
2014-04-10 16:49:31 -0400

    Everything works

--- src/lab3.py
+++ src/lab3.py
@@ -73,7 +73,6 @@ def AStar_search(start, end):
             for frontier in FrontierSet:
                 if node.poseEqual(frontier) and repeatedNode_flag == False:
                     new_g = current.g + move_cost(current,node)
-                    print str(new_g) + '  ' + str(node.g)
                     if node.g > new_g:
                         node.g = new_g
                         node.h = heuristic(node, end)
@@ -105,32 +104,42 @@ def getMapIndex(node):
 #Takes in current node, returns list of possible directional movements
 def WhereToGo(node):
     possibleNodes = []
+    
+    direction = Direction()
 
     #Hacky code begins
     North = AStarNode(round(node.point.x,1), round(node.point.y+0.2,1))
     North.g = node.g + 0.2
+    North.step_direction = direction.n
     
     NorthEast = AStarNode(round(node.point.x+0.2,1), round(node.point.y+0.2,1))
     NorthEast.g = node.g + 0.29
+    NorthEast.step_direction = direction.ne
     
-    East = AStarNode(round(node.point.x+0.2,1), node.point.y)
+    East = AStarNode(round(node.point.x+0.2,1), round(node.point.y, 1))
     East.g = node.g + 0.2
+    East.step_direction = direction.e
     
     SouthEast = AStarNode(round(node.point.x+0.2,1), round(node.point.y-0.2,1))
     SouthEast.g = node.g + 0.29
+    SouthEast.step_direction = direction.se
     
-    South = AStarNode(node.point.x, round(node.point.y-0.2,1))
+    South = AStarNode(round(node.point.x, 1), round(node.point.y-0.2,1))
     South.g = node.g + 0.2
+    South.step_direction = direction.s
     
     SouthWest = AStarNode(round(node.point.x-0.2,1), round(node.point.y-0.2,1))
     SouthWest.g = node.g + 0.29
-   
-    West = AStarNode(round(node.point.x-0.2,1), node.point.y)
+    SouthWest.step_direction = direction.sw
+    
+    West = AStarNode(round(node.point.x-0.2,1), round(node.point.y, 1))
     West.g = node.g + 0.2
+    West.step_direction = direction.w
     
     NorthWest = AStarNode(round(node.point.x-0.2,1), round(node.point.y+0.2,1))
     NorthWest.g = node.g + 0.29
-
+    NorthWest.step_direction = direction.nw
+    
     if map_data[getMapIndex(North)] != 100:
         possibleNodes.append(North)
     if map_data[getMapIndex(NorthEast)] != 100:
@@ -148,6 +157,7 @@ def WhereToGo(node):
     if map_data[getMapIndex(NorthWest)] != 100:
         possibleNodes.append(NorthWest)
 
+    #Hacky code begins
     return possibleNodes
 
 def move_cost(node, next):
@@ -161,15 +171,47 @@ def move_cost(node, next):
 class AStarNode():
     
     def __init__(self, x, y):
+        direction = Direction()
         self.point = Point()
         self.point.x, self.point.y = x, y
         self.g = 0
         self.h = 0
         self.parent = None
+        self.step_direction = direction.start
         
     def poseEqual(self, node):
         return node.point.x == self.point.x and node.point.y == self.point.y 
-   
+    
+
+class Direction():
+    def __init__(self):
+        self.n = 1
+        self.s = 2
+        self.e = 3
+        self.w = 4
+        self.ne = 5
+        self.se = 6
+        self.nw = 7
+        self.sw = 8
+        self.start = 0
+        
+def getWaypoints(path):
+    waypoints = []
+    direction = Direction()
+    previous = direction.start
+    
+    for node in path:
+        if(node.step_direction != previous):
+            waypoints.append(node.parent)
+            previous = node.step_direction
+        else:
+            continue
+    
+    waypoints.append(start)
+    waypoints.append(end)
+        
+    return waypoints    
+
 #Publish Explored Cells function
 def PublishGridCells(publisher, nodes):
     #Initialize gridcell
@@ -187,92 +229,112 @@ def PublishGridCells(publisher, nodes):
         point.z = node.point.z = 0
         gridcells.cells.append(point)        
     publisher.publish(gridcells)
-    rospy.sleep(rospy.Duration(0.01,0))
+    rospy.sleep(rospy.Duration(0.05,0))
+
+def run_Astar():
     
+    PublishGridCells(pub_start, [start])
+    PublishGridCells(pub_end, [end])
+    PublishGridCells(pub_path, [])
+    
+    # Use this command to make the program wait for some seconds
+    rospy.sleep(rospy.Duration(1, 0))
+    
+    path = AStar_search(start, end)
+    
+    print "Displaying Path"
+    PublishGridCells(pub_path, path)
+    
+    blah = raw_input("Press enter to display ")
+    PublishGridCells(pub_path, [])
+    
+    print "Showing Waypoints"
+    waypoints = getWaypoints(path)
+    rospy.sleep(rospy.Duration(1,0))
+    PublishGridCells(pub_path, waypoints)
 
 #####################################3
 # set and print initialpose
 def set_initial_pose (msg):
-	
-	global start_pos_x
-	global start_pos_y
-	#global start_pos_z
-	#global start_orient_x
-	#global start_orient_y
-	#global start_orient_z
-	#global start_w
-
-	#set initial pose values
-	if msg.pose.pose.position.x % .2 < .1:
-		start_pos_x = (msg.pose.pose.position.x / .2)
-	else:
-		start_pos_x = (msg.pose.pose.position.x / .2) + .2
-
-	if msg.pose.pose.position.y % .2 < .1:
-		start_pos_y = (msg.pose.pose.position.y / .2)
-	else:
-		start_pos_y = (msg.pose.pose.position.y / .2) + .2
-
-	#if msg.pose.pose.position.x % .2 < .1:
-	#	start_pos_x = (msg.pose.pose.position.x / .2)
-	#else:
-	#	start_pos_x = (msg.pose.pose.position.x / .2) + .2
-
-	#we have not checked scaling or anything for these:
-	#start_orient_x = msg.pose.pose.orientation.x
-	#start_orient_y = msg.pose.pose.orientation.y
-	#start_orient_z = msg.pose.pose.orientation.z
-	#start_w = msg.pose.pose.orientation.w
-	
-	#print initial pose values
-	print ""
-	print "Initial Pose Values:"
-	print "start_pos_x = ", start_pos_x
-	print "start_pos_y = ", start_pos_y
-	#print "start_pos_z = ", start_pos_z
-
-	#we have not checked scaling or anything for these:
-	#print "start_orient_x = ", start_orient_x
-	#print "start_orient_y = ", start_orient_y
-	#print "start_orient_z = ", start_orient_z
-	#print "start_w = ", start_w
+    
+    global start_pos_x
+    global start_pos_y
+    global start_pos_flag
+    #global start_pos_z
+    #global start_orient_x
+    #global start_orient_y
+    #global start_orient_z
+    #global start_w
+    
+    start_pos_x = msg.pose.pose.position.x
+    start_pos_y = msg.pose.pose.position.y
+    
+    #set initial pose values
+    if msg.pose.pose.position.x % .2 < .1:
+    	start_pos_x = start_pos_x - (msg.pose.pose.position.x % .2)
+    else:
+    	start_pos_x = start_pos_x - (msg.pose.pose.position.x % .2) + .2
+    
+    if msg.pose.pose.position.y % .2 < .1:
+    	start_pos_y = start_pos_y - (msg.pose.pose.position.y % .2)
+    else:
+    	start_pos_y = start_pos_y - (msg.pose.pose.position.y % .2) + .2
+    
+    #if msg.pose.pose.position.x % .2 < .1:
+    #	start_pos_x = (msg.pose.pose.position.x / .2)
+    #else:
+    #	start_pos_x = (msg.pose.pose.position.x / .2) + .2
+    
+    #we have not checked scaling or anything for these:
+    #start_orient_x = msg.pose.pose.orientation.x
+    #start_orient_y = msg.pose.pose.orientation.y
+    #start_orient_z = msg.pose.pose.orientation.z
+    #start_w = msg.pose.pose.orientation.w
+    
+    #print initial pose values
+    start.point.x = start_pos_x
+    start.point.y = start_pos_y
+    print ""
+    print "Initial Pose Values:"
+    print "start_pos_x = ", start.point.x
+    print "start_pos_y = ", start.point.y
+    #print "start_pos_z = ", start_pos_z
+    
+   
+    
+    start_pos_flag = True
+    #we have not checked scaling or anything for these:
+    #print "start_orient_x = ", start_orient_x
+    #print "start_orient_y = ", start_orient_y
+    #print "start_orient_z = ", start_orient_z
+    #print "start_w = ", start_w
 
 # set and print goalpose
 def set_goal_pose (msg):
-
-	global goal_pos_x
-	global goal_pos_y
-	#global goal_pos_z
-	#global goal_orient_x
-	#global goal_orient_y
-	#global goal_orient_z
-	#global goal_w
-
-	#set goal pose values
-	goal_pos_x = msg.pose.position.x
-	goal_pos_y = msg.pose.position.y
-	#goal_pos_z = msg.pose.position.z
-	
-	#we have not checked scaling or anything for these:
-	#goal_orient_x = msg.pose.orientation.x
-	#goal_orient_y = msg.pose.orientation.y
-	#goal_orient_z = msg.pose.orientation.z
-	#goal_w = msg.pose.orientation.w
-	
-	#print goal pose values
-	print ""
-	print "Goal Pose Values:"
-	print "goal_pos_x = ", goal_pos_x
-	print "goal_pos_y = ", goal_pos_y
-	#print "goal_pos_z = ", goal_pos_z
-
-	#we have not checked scaling or anything for these:
-	#print "goal_orient_x = ", goal_orient_x
-	#print "goal_orient_y = ", goal_orient_y
-	#print "goal_orient_z = ", goal_orient_z
-	#print "goal_w = ", goal_w
-
-
+    
+    global end_pos_flag
+    end_pos_x = msg.pose.position.x
+    end_pos_y = msg.pose.position.y
+    #set initial pose values
+    if msg.pose.position.x % .2 < .1:
+        end_pos_x = end_pos_x - (msg.pose.position.x % .2)
+    else:
+        end_pos_x = end_pos_x - (msg.pose.position.x % .2) + .2
+    
+    if msg.pose.position.y % .2 < .1:
+        end_pos_y = end_pos_y - (msg.pose.position.y % .2)
+    else:
+        end_pos_y = end_pos_y - (msg.pose.position.y % .2) + .2
+        
+        
+    end.point.x = end_pos_x
+    end.point.y = end_pos_y
+    
+    print "end_pos_x = ", end.point.x
+    print "end_pos_y = ", end.point.y 
+    
+    end_pos_flag = True
+    
 
 #######################################
 # This is the program's main function
@@ -296,8 +358,10 @@ if __name__ == '__main__':
     global Map_Cell_Height
     global map_data
     
-    global start_pos_x
-    global start_pos_y
+    global start_pos_flag
+    global end_pos_flag
+    
+   
     #global start_pos_z
     #global start_orient_x
     #global start_orient_y
@@ -326,26 +390,25 @@ if __name__ == '__main__':
     sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, set_goal_pose, queue_size=1)  
     sub = rospy.Subscriber('/map', OccupancyGrid, map_function, queue_size=1)
     
+    start_pos_flag = False
+    end_pos_flag = False
     # Use this command to make the program wait for some seconds
     rospy.sleep(rospy.Duration(1, 0))
     
     print "Starting Lab 3"
-    # Hardcoded start and end points: 
-    start  = AStarNode(-1, -1.6)
-    end    = AStarNode(1.4, 2.2)
-    PublishGridCells(pub_start, [start])
-    PublishGridCells(pub_end, [end])
-    PublishGridCells(pub_path, [])
+    start = AStarNode(-1,-1.8)
+    end = AStarNode(1,1.8);
+    while(1):
+        if(start_pos_flag == True and end_pos_flag == True):
+            print 'running Astar'
+            run_Astar();
+            start_pos_flag = False
+            end_pos_flag = False
+            
+    print "Lab Complete"
+
+
     
-    # Use this command to make the program wait for some seconds
-    rospy.sleep(rospy.Duration(1, 0))
-    #PublishGridCells(pub_frontier,  WhereToGo(start))
-    path = AStar_search(start, end)
-    print path[len(path) - 1].g
-    PublishGridCells(pub_path, path)
-    
-    
-    print "Lab 3 complete!"
-    rospy.spin()
-    
+
+        
     
